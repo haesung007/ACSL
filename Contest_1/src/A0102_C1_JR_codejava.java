da8b0336c5e57bf49d0db9b9cde72aa6ab637aa8
haesung007 <142755084+haesung007@users.noreply.github.com>
2023-08-20 18:24:17 -0400

    s..

---
+++ Contest_1/src/A0102_C1_JR_codejava.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class A0102_C1_JR_codejava {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int n = in.next().charAt(0) - 64;
+		
+		System.out.println(n);
+		
+		
+		System.out.println((char)n+64);
+		
+	}
+
+}
